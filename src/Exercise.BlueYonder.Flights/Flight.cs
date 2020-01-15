8362b8478422fa4d45826f5806778c650c030c68
fagnertonon <fagnertonon@hotmail.com>
2020-01-15 12:22:51 -0300

    add modulo 1

---
+++ src/Exercise.BlueYonder.Flights/Flight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise.BlueYonder.Flights
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string FlightNumber { get; set; }
+        public DateTime DepartureTime { get; set; }
+    }
+}
---
+++ src/Exercise.BlueYonder.Flights/FlightsContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise.BlueYonder.Flights
+{
+    public class FlightsContext : DbContext
+    {
+        public FlightsContext(DbContextOptions<FlightsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+    }
+}
